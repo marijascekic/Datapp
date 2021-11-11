c85c1f583584acf3d524d1595c5fce5d11b2d351
M <marijascekic1982@gmail.com>
2021-11-11 22:22:55 +0100

    Fit commit

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import yfinance as yf
 import streamlit as st
-
-st.title("Hello!")
+st.write("""
+#  Stock closing price of Tesla """)
+tickerSymbol = 'TSLA'
+tickerData = yf.Ticker(tickerSymbol)
+from datetime import date
+today = date.today().strftime('%Y-%m-%d')
+tickerDf = tickerData.history(period='1d', start='2021-10-1', end=today)
+st.line_chart(tickerDf.Close)
