2ad80fd8956031e6b2cc0923e10b6ee2164572bf
M <marijascekic1982@gmail.com>
2021-11-11 21:30:29 +0100

    Fit commit

--- Bitcoin.py
+++ Bitcoin.py
@@ -1,24 +1,3 @@
-import sys
-
-if not sys.warnoptions:
-    import warnings
-    warnings.simplefilter("ignore")
-    
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-from datetime import date
-today = date.today().strftime('%d/%m/%Y')
-
-import  investpy
-data = investpy.get_crypto_historical_data(crypto='bitcoin', from_date='1/10/2021', to_date=today)
-
-data2=data.iloc[:,3]
-
-
-#data = investpy.get_crypto_recent_data(crypto='bitcoin')['Close']
- 
-#df=pd.read_excel(r'C:\Users\Korisnik\Desktop\SVE\app\Bitcoin.xlsx' ) 
-
-st.bar_chart(data2)
+st.title("Hello!")
